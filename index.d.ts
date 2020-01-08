151dd3b413aa6a713fcdc0880749895d7af1786d
Thomas Egain <thomas.egain@gmail.com>
2020-01-08 12:20:44 +0100

    update defs file

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,3 @@
-/**
- * @typedef {Object} Product
- *
- * @property {string} _id
- * @property {string} name
- * @property {string} description
- * @property {string} review
- * @property {string} pictureUrl
- * @property {number} rating
- * @property {number} price
- * @property {'Male'|'Female'} gender
- */
 export interface Product {
   _id: string;
   name: string;
@@ -21,26 +9,12 @@ export interface Product {
   gender: 'Male' | 'Female';
 }
 
-/**
- * @typedef {Object} ItemQuantity
- * @property {number} quantity
- * @typedef {Product & ItemQuantity} CartItem
- */
 export interface CartItem extends Product {
   quantity: number;
 }
 
-/**
- * @typedef {CartItem[]} Cart
- */
 export type Cart = CartItem[];
 
-/**
- * @typedef {Object} State
- *
- * @property {Product[]} products
- * @property {CartItem[]} cart
- */
 export interface State {
   products: Product[];
   cart: Cart;
